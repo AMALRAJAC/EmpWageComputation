e4e34116a0a123ec66479ade3de95146b0760b46
unknown <acamalraj@gmail.com>
2021-03-20 08:24:04 +0530

    add program to  compute employee wage for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,19 +1,25 @@
 #!/bin/bash -x
-is_fulltime=1
-is_parttime=2
+is_parttime=1
+is_fulltime=2
 emp_rate_per_hour=20
+total_working_days=20
+total_salery=0
+ for (( day=0;day<=$total_working_days;day++ ))
+do
+        emp_check=$((RANDOM%3))
+        case "$emp_check" in
 
-emp_check=$((RANDOM%3))
-case "$emp_check" in
-$is_fulltime)
-        emp_hour=8
-;;
-$is_parttime)
-        emp_hour=4
-;;
-*)
-        emp_hour=0
-;;
-esac
-salery=$(( $emp_rate_per_hour*$emp_hour ))
+                $is_fulltime)
+                        emp_hours=8
+                ;;
+                $is_parttime)
+                        emp_hours=4
+                ;;
+                *)
+                        emp_hours=0
+                ;;
+        esac
+        salery=$(($emp_hours*$emp_rate_per_hour))
+        total_salery=$(($total_salery+$salery))
+done
 
