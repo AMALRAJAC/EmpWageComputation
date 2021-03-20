db4e189d868031854c1fad6b729efb864b47e305
unknown <acamalraj@gmail.com>
2021-03-20 08:09:46 +0530

    add program to  compute parttime and full time employee wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -x
-is_present=1
-emp_check=$((RANDOM%2))
-if [ $is_present -eq $emp_check ]
+is_fulltime=1
+is_parttime=2
+ emp_check=$((RANDOM%3))
+ emp_rate_per_hour=20
+if [ $is_fulltime -eq $emp_check ]
 then
-        emp_rate_per_hour=20
-        emp_working_hr=8
-        salery=$(( $emp_rate_per_hour*$emp_working_hr ))
+        echo "employee is full time"
+        emp_working_hour=8
+elif [ $is_parttime -eq $emp_check ]
+then
+        echo "employee is parttime"
+        emp_working_hour=4
 else
-        salery=0
+        echo "employee is absent"
+        emp_working_hour=0
 fi
+salery=$(( $emp_rate_per_hour*$emp_working_hour ))
+
 
 
